3ce1d5fa72ca2ce9d689c849986b34197f4fba37
Tiago Inácio <tiago.inacio@farfetch.com>
2017-12-29 18:21:43 +0000

    update linkedlist tail class name

--- src/data-structures/linked-list-tail.h
+++ src/data-structures/linked-list-tail.h
@@ -16,18 +16,18 @@ struct Node {
 };
 
 template <typename T>
-class LinkedList {
+class LinkedListTail {
  private:
     datastructures::Node<T>* head_;
     datastructures::Node<T>* tail_;
     size_t size_;
 
  public:
-    LinkedList() :
+    LinkedListTail() :
         head_(nullptr),
         size_(0) {}
 
-    ~LinkedList() {
+    ~LinkedListTail() {
         if (head_ != nullptr) {
             while (head_->next != nullptr) {
                 datastructures::Node<T>* tmp = head_;
@@ -41,7 +41,7 @@ class LinkedList {
         }
     }
 
-    LinkedList<T>(const LinkedList<T>& other) {
+    LinkedListTail<T>(const LinkedListTail<T>& other) {
         datastructures::Node<T>* current = other.head_;
         datastructures::Node<T>* newNode = new datastructures::Node<T>();
         head_ = newNode;
@@ -59,7 +59,7 @@ class LinkedList {
     }
 
     // assignment operator
-    LinkedList<T>& operator=(const LinkedList<T>& rhs) {
+    LinkedListTail<T>& operator=(const LinkedListTail<T>& rhs) {
         if (this != &rhs) {
             auto tmp(rhs);
             std::swap(head_, tmp.head_);
