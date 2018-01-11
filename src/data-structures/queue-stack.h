1541c43c57cb5263841fff11f5733d6f02dd0625
Tiago Inácio <tiago.inacio@blip.pt>
2018-01-11 22:47:15 +0000

    add queue stack

--- src/data-structures/queue-stack.h
+++ src/data-structures/queue-stack.h
@@ -12,27 +12,52 @@ class QueueStack {
  private:
     datastructures::StackList<T> firstStack_;
     datastructures::StackList<T> secondStack_;
+    bool isSecondStackEmpty;
  public:
     QueueStack() :
+        firstStack_(datastructures::StackList<T>()),
+        secondStack_(datastructures::StackList<T>()),
+        isSecondStackEmpty(true)
         {}
 
-    ~QueueStack() {
-    }
-
     bool isEmpty() const {
-    }
-
-    bool isFull() const {
+        return firstStack_.isEmpty() && secondStack_.isEmpty();
     }
 
     void enqueue(const T &data) {
-        // push to stack
+        if (isSecondStackEmpty == false) {
+            while (!secondStack_.isEmpty()) {
+                firstStack_.push_back(secondStack_.pop());
+            }
+            isSecondStackEmpty = true;
+        }
+        firstStack_.push_back(data);
     }
 
-    T peek() const {
+    T peek() {
+        if (isSecondStackEmpty == true) {
+            while (!firstStack_.isEmpty()) {
+                secondStack_.push_back(firstStack_.pop());
+            }
+
+            isSecondStackEmpty = false;
+        }
+
+        T value = secondStack_.peek();
+
+        return value;
     }
 
     T dequeue() {
+        if (isSecondStackEmpty == true) {
+            while (!firstStack_.isEmpty()) {
+                secondStack_.push_back(firstStack_.pop());
+            }
+
+            isSecondStackEmpty = false;
+        }
+
+        return secondStack_.pop();
     }
 };
 }
