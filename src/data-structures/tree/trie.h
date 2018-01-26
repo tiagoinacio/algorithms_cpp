51cc1bd319ac0d41896bd30110f24f2a5ea90434
Tiago Inácio <tiago.inacio@farfetch.com>
2018-01-26 17:56:49 +0000

    udpate trie implementatino

--- src/data-structures/tree/trie.h
+++ src/data-structures/tree/trie.h
@@ -3,19 +3,20 @@
 
 #include <memory>
 #include "data-structures/hash-table/open-addressing.h"
+#include "data-structures/queue/queue-list.h"
 
 namespace datastructures {
 
-struct TrieNode {
-    std::string value;
-    bool isWord;
-    datastructures::HashTable<TrieNode*> childrens;
+const size_t ALPHABET_SIZE = 26;
 
-    TrieNode(const std::string& value_)
+struct TrieNode {
+    bool isEndOfWord;
+    TrieNode* childrens[ALPHABET_SIZE]; // alphabet size
+
+    TrieNode()
         :
-        value(value_),
-        isWord(false),
-        childrens(datastructures::HashTable<TrieNode*>())
+        isEndOfWord(false),
+        childrens()
         {}
 };
 
@@ -24,43 +25,102 @@ class Trie {
  private:
     TrieNode* root_;
 
+    TrieNode* findNode(const T& word) {
+        TrieNode* node = root_;
+
+        if (node == nullptr) {
+            return nullptr;
+        }
+
+        for (const char i : word) {
+            size_t index = i - 'a';
+
+            if (node->childrens[index] != nullptr) {
+                node = node->childrens[index];
+            } else {
+                return nullptr;
+            }
+        }
+        return node;
+    }
+
  public:
     Trie<T>()
     :
-    root_(nullptr)
+    root_(new TrieNode())
     {}
 
     ~Trie<T>() {
+        deleteNode(root_);
     }
 
-    // should validate if it is valid char of alphabet
-    void insert(const T& value) {
-        TrieNode* node = root_;
+    void deleteNode(TrieNode* node) {
+        auto queue = datastructures::QueueList<TrieNode*>();
 
-        for (auto i : value) {
-            const std::string e = std::string(1, i);
+        if (node == nullptr) {
+            return;
+        }
 
-            if (node == nullptr) {
-                node = new TrieNode(e);
-                root_ = node;
-            } else {
-                if (node->childrens.exists(e)) {
-                    node = node->childrens.get(e);
-                } else {
-                    TrieNode* children = new TrieNode(e);
-                    node->childrens.set(e, children);
-                    node = children;
+        queue.enqueue(node);
+
+        while (!queue.isEmpty()) {
+            auto node = queue.dequeue();
+
+            for (auto i = 0; i < ALPHABET_SIZE; ++i) {
+                if (node->childrens[i] != nullptr) {
+                    queue.enqueue(node->childrens[i]);
+                    node->childrens[i] = nullptr;
                 }
             }
+
+            if (node == root_) {
+                root_ = nullptr;
+            }
+
+            delete node;
         }
     }
 
-    void deleteNode() {
+    // should validate if it is valid char of alphabet
+    void insert(const T& word) {
+        TrieNode* node = root_;
 
+        for (const char i : word) {
+            // if 'a', then is inserted at position 0,
+            // if 'b', then is inserted at position 1,
+            // and so on, until reach the limit of 26
+            size_t index = i - 'a';
+
+            if (node->childrens[index] != nullptr) {
+                node = node->childrens[index];
+            } else {
+                TrieNode* children = new TrieNode();
+                node->childrens[index] = children;
+                node = children;
+            }
+        }
+
+        node->isEndOfWord = true;
     }
 
-    void search() {
+    bool contains(const T& word) {
+        TrieNode* node = findNode(word);
 
+        if (node == nullptr) {
+            return false;
+        }
+
+        return node->isEndOfWord;
+    }
+
+    bool isPrefix(const T& word) {
+        TrieNode* node = findNode(word);
+
+        if (node == nullptr) {
+            return false;
+        }
+
+        return !node->isEndOfWord;
     }
 
     TrieNode* root() {
--- test/data-structures/tree/trie.cpp
+++ test/data-structures/tree/trie.cpp
@@ -2,12 +2,49 @@
 #include <string>
 #include "data-structures/tree/trie.h"
 
-TEST(trie, insert) {
+TEST(trie, constructor) {
+    datastructures::Trie<std::string> trie;
+
+    ASSERT_TRUE(trie.root() != nullptr);
+    ASSERT_TRUE(trie.root()->isEndOfWord == false);
+}
+
+TEST(trie, insert_contains) {
     datastructures::Trie<std::string> trie;
 
     trie.insert("tiago");
 
-    ASSERT_STREQ(trie.root()->value.c_str(), "t");
-    ASSERT_STREQ(trie.root()->childrens.get("t")->value.c_str(), "i");
-    ASSERT_STREQ(trie.root()->childrens.get("t")->childrens.get("i")->value.c_str(), "a");
+    ASSERT_EQ(trie.contains("t"), false);
+    ASSERT_EQ(trie.contains("ti"), false);
+    ASSERT_EQ(trie.contains("tia"), false);
+    ASSERT_EQ(trie.contains("tiag"), false);
+    ASSERT_EQ(trie.contains("tiago"), true);
+}
+
+TEST(trie, isPrefix) {
+    datastructures::Trie<std::string> trie;
+
+    trie.insert("tiago");
+
+    ASSERT_EQ(trie.isPrefix("t"), true);
+    ASSERT_EQ(trie.isPrefix("ti"), true);
+    ASSERT_EQ(trie.isPrefix("tia"), true);
+    ASSERT_EQ(trie.isPrefix("tiag"), true);
+    ASSERT_EQ(trie.isPrefix("tiago"), false);
+}
+
+TEST(trie, deleteNode) {
+    datastructures::Trie<std::string> trie;
+
+    trie.insert("tiago");
+
+    ASSERT_EQ(trie.contains("tiago"), true);
+
+    trie.deleteNode(trie.root());
+
+    ASSERT_EQ(trie.contains("t"), false);
+    ASSERT_EQ(trie.contains("ti"), false);
+    ASSERT_EQ(trie.contains("tia"), false);
+    ASSERT_EQ(trie.contains("tiag"), false);
+    ASSERT_EQ(trie.contains("tiago"), false);
 }
